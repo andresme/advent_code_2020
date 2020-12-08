f7530762944e4ba13f83bf15c32eca7f9bbaefaa
Andres <andres.me@gmail.com>
2020-12-08 08:13:01 -0600

    day 8 puzzle

---
+++ day_8/day8.py
@@ -0,0 +1,36 @@
+def run_program(instructions):
+    pointers_processed = set()
+    accumulator = 0
+    pointer = 0
+    while pointer not in pointers_processed and pointer < len(instructions):
+        pointers_processed.add(pointer)
+        operation, argument = instructions[pointer].split(' ')
+        if operation == 'acc':
+            accumulator += int(argument)
+            pointer += 1
+        elif operation == 'jmp':
+            pointer += int(argument)
+        else:
+            pointer += 1
+    return accumulator, pointer
+
+if __name__ == "__main__":
+    with open('day_8/input.txt', 'r') as inputFile:
+        instructions = inputFile.read().split('\n')
+    
+    print(run_program(instructions)[0])
+
+    for i in range(len(instructions)):
+        instructions_temp = instructions.copy()
+        operation, argument = instructions_temp[i].split(' ')
+        if operation == 'jmp':
+            instructions_temp[i]  = ' '.join(['nop', argument])
+        elif operation == 'nop':
+            instructions_temp[i]  = ' '.join(['jmp', argument])
+        else:
+            continue
+
+        accum, pointer = run_program(instructions_temp)
+        if pointer == len(instructions):
+            print(accum)
+   
